6bde09f3a205688b8b5c7a869cad1c619980845c
Justis Lamanna <justislamanna@JustisLmannaMBP.amer.csc.com>
2017-03-10 10:49:11 -0600

    Finished client-side validation for Registration.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -15,10 +15,12 @@ export class RegisterComponent implements OnInit {
 
   constructor(fb: FormBuilder) {
     this.regForm = fb.group({
-      'user': ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
-      'pass': ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
-      'confpass': ['', Validators.required]
+      'email': [''],
+      'user': [''],
+      'pass': [''],
+      'confpass': ['']
     })
+    this.regForm.controls['confpass'].setValidators(RegisterComponent.checkEquals(this.regForm.controls['pass']));
     this.close = new EventEmitter();
     this.errors = new Array();
   }
@@ -32,14 +34,24 @@ export class RegisterComponent implements OnInit {
    */
   onRegister(form: any){
     this.errors.splice(0, this.errors.length); //Clear the error log.
+    let email = this.regForm.controls['email'];
     let user = this.regForm.controls['user'];
     let pass = this.regForm.controls['pass'];
     let pass2 = this.regForm.controls['confpass'];
+    if(email.hasError('required')){
+      this.errors.push('An email must be specified.');
+    }
+    else if(email.hasError('pattern')){
+      this.errors.push('Email must be in the form of [name]@[domain].[suffix].')
+    }
     if(user.hasError('required')){
       this.errors.push('A username must be specified.');
     }
     else if(user.hasError('minlength') || user.hasError('maxlength')){
       this.errors.push('Username must be between 6 and 20 characters.');
+    }
+    else if(user.hasError('pattern')){
+      this.errors.push('Username can only contain letters, numbers, underscores, or dashes.');
     }
     if(pass.hasError('required')){
       this.errors.push('A password must be specified.');
@@ -47,14 +59,31 @@ export class RegisterComponent implements OnInit {
     else if(pass.hasError('minlength') || pass.hasError('maxlength')){
       this.errors.push('Password must be between 6 and 20 characters.');
     }
+    if(pass2.hasError('notequal')){
+      this.errors.push('Passwords do not match.');
+    }
+    //If we missed an error somehow.
     if(this.errors.length == 0 && !form.valid){
       this.errors.push('An error occured processing your registration.');
     }
     console.log(form);
   }
 
+  /**
+   * Handles any code from this side that happens when a close event is triggered.
+   */
   closeModal(){
     this.errors.splice(0, this.errors.length);
     this.close.emit('');
+  }
+
+  /**
+   * Compares two fields for equality.
+   * @param group The group to compare to this one for equality.
+   */
+  static checkEquals(group: AbstractControl): ValidatorFn{
+    return (c: AbstractControl) => {
+      return group.value == c.value ? null : {notequal: true};
+    };
   }
 }
