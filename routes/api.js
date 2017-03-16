7d6dc1d4c602977034e1e8faa36edbfcfc1d4d5a
Justis Lamanna <lamanna.justis@gmail.com>
2017-03-16 10:23:29 -0500

    Modified registration endpoint Now operates in a proper REST format.

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://GlazedSite:gpword0714@localhost:27017/glazed');
+var crypto = require('password-hash-and-salt');
 
 router.get('/users', function(req, res, next){
     db.users.find(function(err, users){
@@ -40,16 +41,50 @@ router.get('/users/username/:id', function(req, res, next){
     });
 });
 
-router.post('/users/register', function(req, res, next){
-    let username = req.body.username;
-    let email = req.body.email;
-    let pass = req.body.password;
+router.post('/users', function(req, res, next){
+    let username = String(req.body.username);
+    let email = String(req.body.email);
+    let pass = String(req.body.password);
     if(username == null || email == null || pass == null){
         res.json({error: true, reason: "Missing Credentials"});
     }
-    else{
-        res.json({error: false, reason: null});
+    else if(username.length < 6 || username.length > 20){
+        res.json({error: true, reason: "Username not the correct size."});
     }
-})
+    else if(!username.match(/^[A-Za-z0-9\-_]*$/)){
+        res.json({error: true, reason: "Invalid username."});
+    }
+    else if(email.length > 50){
+        res.json({error: true, reason: "Email too long."});
+    }
+    else if(!email.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9\-.]+$/)){
+        res.json({error: true, reason: "Invalid email"});
+    }
+    else if(pass.length < 6 || pass.length > 20){
+        res.json({error: true, reason: "Password not the correct size."});
+    }
+    else{
+        crypto(pass).hash(function(error, hash){
+            if(error){
+                res.json({error: true, reason: "Error with password hash."});
+            }
+            else{
+                let cred = {
+                    username: username,
+                    email: email,
+                    pass: hash
+                };
+                db.users.insert(cred, function(err, result){
+                    if(err){
+                        res.json({error: true, reason: "Error storing in database."});
+                    }
+                    else{
+                        res.json({error: false, reason: null});
+                    }
+                });
+            }
+        });
+    }
+});
 
 module.exports = router;
