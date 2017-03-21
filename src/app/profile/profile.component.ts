005d99b1ef90f4f5298eb214305ba4190ab05a8e
Justis Lamanna <lamanna.justis@gmail.com>
2017-03-21 09:45:58 -0500

    Profile can now be updated.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { InfoService } from '../services/info.service';
 import { LoginService } from '../services/login.service';
+import { FormGroup } from '@angular/forms';
+import { ModalDirective } from 'ng2-bootstrap';
 
 @Component({
   selector: 'app-profile',
@@ -8,8 +10,10 @@ import { LoginService } from '../services/login.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('loginModal') loginModal: ModalDirective;
   user: any;
   edit: boolean;
+
   aboutme = "";
 
   constructor(private info: InfoService, private login: LoginService) { }
@@ -17,9 +21,56 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.info.getInfoOn(this.login.getUserID()).then((user) => {
       this.user = user;
-      if(user.bio){
-        this.aboutme = user.bio;
+      this.aboutme = user.bio || '';
+    });
+  }
+
+  onSubmit(form: FormGroup){
+    let newbio = {
+      bio: form['bio'],
+      location: form['location'],
+      fav: {
+        pkmn: form['pkmn'],
+        move: form['move'],
+        ability: form['ability'],
+        nature: form['nature'],
+        game: form['game']
+      }
+    };
+    this.submitValues(newbio);
+  }
+
+  onModalSubmit(form: FormGroup){
+    this.onSubmit(form);
+  }
+
+  submitValues(newbio: any){
+    this.info.needLogin().then((res: boolean) => {
+      if(res){
+        this.info.setInfoOn(this.login.getUserID(), newbio).then((res: boolean) => {
+          if(res){
+            this.edit = false;
+            this.user.bio = newbio.bio;
+            this.user.location = newbio.location;
+            this.user.fav = newbio.fav;
+          }
+          else{
+            this.loginModal.show();
+          }
+        });
+      }
+      else{
+        this.loginModal.show();
       }
     });
+  }
+
+  onCancel(form: FormGroup){
+    form.reset();
+    this.edit = false;
+  }
+
+  onModalClose(form: FormGroup){
+    this.onCancel(form);
   }
 }
--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -22,4 +22,40 @@ export class InfoService {
       });
     });
   }
+
+  /**
+   * Update a user's info.
+   * @param uid The user's ID.
+   * @param update The update string.
+   */
+  setInfoOn(uid: String, update: any): Promise<boolean>{
+    return new Promise((resolve, reject) => {
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('x-access-token', this.login.getToken());
+      this.http.post('/api/v1/users/id/' + uid, update, {headers: headers}).subscribe((res: Response) => {
+        let obj = res.json();
+        resolve(obj.success);
+      });
+    });
+  }
+
+  /**
+   * Check if a login needs to occur.
+   * Also refreshes the token if a login isn't necessary.
+   */
+  needLogin(): Promise<boolean>{
+    return new Promise((resolve, reject) => {
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('x-access-token', this.login.getToken());
+      this.http.post('/api/v1/users/refresh', {}, {headers: headers}).subscribe((res: Response) => {
+        let obj = res.json();
+        if(obj.success){
+          this.login.setCredentials(obj.token, this.login.getUsername(), this.login.getUserID());
+        }
+        resolve(obj.success);
+      });
+    });
+  }
 }
