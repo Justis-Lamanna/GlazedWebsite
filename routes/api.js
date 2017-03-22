e18eeb094067631cbe749d33d1eb1f279e93224e
Justis Lamanna <lamanna.justis@gmail.com>
2017-03-22 17:00:37 -0500

    Started adding the ability to add and remove games

--- routes/api.js
+++ routes/api.js
@@ -231,6 +231,42 @@ router.post('/users', function(req, res, next){
     }
 });
 
+//Add a game.
+router.post('/games', function(req, res, next){
+    let g = req.body;
+    let game = {
+        gamename: g.gamename,
+        name: g.name,
+        gender: g.gender,
+        seen: g.seen,
+        caught: g.caught,
+        location: g.location,
+        badges: g.badges
+    };
+    db.games.insert(game, function(err, result){
+        if(err){
+            console.log("Error in database: " + err);
+            res.json({success: false, reason: "Error storing in database."});
+        }
+        else{
+            console.log("Game registration successful.");
+            res.json({success: true, gameid: result._id});
+        }
+    });
+});
+
+//Get all games.
+router.get('/games', verify, verifyAdmin, function(req, res, next){
+    db.games.find(function(err, games){
+        if(err){
+            res.json({success: false, reason: "Error reading database."});
+        }
+        else{
+            res.json({success: true, data: games});
+        }
+    })
+})
+
 //Anything that does not match gets a 403 Forbidden page.
 router.get('*', function(req, res, next){
     res.status(403).send('Forbidden');
