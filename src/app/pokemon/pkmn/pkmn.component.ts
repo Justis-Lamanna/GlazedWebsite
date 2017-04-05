a5d34367098f976615ceb73523510d6eff41f181
Justis Lamanna <lamanna.justis@gmail.com>
2017-04-05 16:45:32 -0500

    Added Pokemon tiles to the profile page.

--- src/app/pokemon/pkmn/pkmn.component.ts
+++ src/app/pokemon/pkmn/pkmn.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-pkmn',
@@ -7,8 +9,22 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PkmnComponent implements OnInit {
   @Input() pkmn: any;
-  constructor() { }
+  mouse: boolean;
+  constructor(private route : Router, private prof: ProfileService) { }
 
   ngOnInit() {
+  }
+
+  //This should really be a thing.
+  titlecase(word: string): string{
+    let split = word.split(' ');
+    let done = '';
+    for(var index = 0; index < split.length; index++){
+      if(index != 0){
+        done += ' ';
+      }
+      done += split[index].slice(0, 1).toUpperCase() + split[index].slice(1);
+    }
+    return done;
   }
 }
